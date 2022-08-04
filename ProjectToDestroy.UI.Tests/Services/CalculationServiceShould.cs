da1f3cfa849ca60f82023cee6b0785f8d28a05f2
Remi Dewinckeleer <remidwr@outlook.com>
2022-08-04 17:01:50 +0200

    Add Calculation and Tests

--- ProjectToDestroy.UI.Tests/Services/CalculationServiceShould.cs
+++ ProjectToDestroy.UI.Tests/Services/CalculationServiceShould.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using ProjectToDestroy.UI.Services;
 
 namespace ProjectToDestroy.UI.Tests.Services
 {
     public class CalculationServiceShould
     {
+        [Theory]
+        [InlineAutoData]
+        [InlineAutoData(-100)]
+        public void GetSum_ReturnsTheSumOf2Numbers(int number1, int number2)
+        {
+            // Arrange
+            var calculation = new CalculationService();
+
+            // Act
+            var result = calculation.GetSum(number1, number2);
+
+            // Assert
+            result.Should().Be(number1 + number2);
+        }
     }
 }
--- ProjectToDestroy.UI/Services/CalculationService.cs
+++ ProjectToDestroy.UI/Services/CalculationService.cs
@@ -2,5 +2,9 @@
 {
     public class CalculationService
     {
+        public int GetSum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
     }
 }
